c202a2ef853444afc5cb4dfdc50a3e0a5406e3e3
BugraKaraca <141683072+karacabugra27@users.noreply.github.com>
2024-10-28 19:40:39 +0300

    alert dialog implementetion

--- app/src/main/java/com/example/turkcellnestedscrollviewlab/MainActivity.kt
+++ app/src/main/java/com/example/turkcellnestedscrollviewlab/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.turkcellnestedscrollviewlab
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun alertEkle(view: View) {
+        var eklemeUyari = AlertDialog.Builder(this)
+        eklemeUyari.setTitle("Uyarı")
+        eklemeUyari.setMessage("Ürünü Eklemek İstiyor Musunuz?")
+
+        eklemeUyari.setPositiveButton("Evet",DialogInterface.OnClickListener { dialogInterface, i ->
+            Toast.makeText(this,"Ürün Eklendi!",Toast.LENGTH_SHORT).show()
+        })
+
+        eklemeUyari.setNegativeButton("Hayır",null)
+
+        var uyari = eklemeUyari.create()
+        uyari.show()
     }
 }
